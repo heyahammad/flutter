a5be8fdd89b7087a57b937ca6e1078616b8e52f8
heyahammad <fahad.ahmed2456@gmail.com>
2025-09-24 05:11:11 +0600

    na

--- khorchapati/lib/model/expense.dart
+++ khorchapati/lib/model/expense.dart
@@ -2,12 +2,19 @@ import 'package:uuid/uuid.dart';
 
 final uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
-  Expense({required this.amount, required this.date, required this.title})
-    : id = uuid.v4();
+  Expense({
+    required this.amount,
+    required this.date,
+    required this.title,
+    required this.category,
+  }) : id = uuid.v4();
 
   final double amount;
   final String title;
   final String id;
   final DateTime date;
+  final Category category;
 }
