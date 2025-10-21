626021cbf8032b0d98d387d44f9e9ac0ca3a28ed
MD. FAISAL AHAMMAD <fahad.ahmed2456@gmail.com>
2025-10-21 12:00:00 +0600

    NA

--- rannaghor/lib/main.dart
+++ rannaghor/lib/main.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 226, 131, 68),
+  ),
+
+  textTheme: GoogleFonts.latoTextTheme(),
+);
 
 void main() {
   runApp(const MyApp());
